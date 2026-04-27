e8d687f281722e53a6a60cfb92cdc6bb63858bef
Ranjan Lamichhane <studylc29@gmail.com>
2026-04-27 11:37:31 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ function assertEnv() {
 
 assertEnv();
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'], credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
